b08144546c87e1a3c80b4ce5ed1415172a4c06ca
eekbk <eekeric@gmail.com>
2023-09-06 12:33:51 -0500

    feat: add click function to Title

--- src/components/projectEntry.js
+++ src/components/projectEntry.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { LaunchOutlined } from '@mui/icons-material'
 import styles from '@/styles/Projects.module.css'
 
 export default function ProjectEntry({ project }) {
@@ -7,7 +8,10 @@ export default function ProjectEntry({ project }) {
 
   return (
     <article className={styles.project}>
-      <h1 className={styles.project__title}>{title}</h1>
+      {url || repo
+        ? <Link href={url ? longUrl : repo } className={styles.project__title}>{title}</Link>
+        : <h1 className={styles.project__title}>{title}</h1> }
+
       {inDev && <h2 className={styles.inDev}>(In Development)</h2>}
         {vidSrc && <video
           className={styles.project__video}
@@ -21,15 +25,17 @@ export default function ProjectEntry({ project }) {
         />}
         {imgSrc && <Image className={styles.project__video} src={imgSrc} width={320} height={180} alt='Spaceballs Now is Now gif' />}
       <div className={styles.project__links}>
+        {url && <Link className={styles.project__link} href={longUrl} target='_blank'>
+            {url}
+            <LaunchOutlined fontSize='inherit'/>
+          </Link>}
       {repo &&
         <Link className={styles.project__link} href={repo} target='_blank'>
-          Github Repo
-          <div>
-            <Image src='/github-logo.png' alt='github icon' width={30} height={30} />
+          <div className={styles.project__githubIconWrapper}>
+            <Image src='/github-logo.png' alt='github icon' width={18} height={18} />
           </div>
-        </Link>}
-      {url && <Link className={styles.project__link} href={longUrl} target='_blank'>
-          {url}
+          Repo
+          <LaunchOutlined fontSize='inherit'/>
         </Link>}
       </div>
       <h3 className={styles.project__description}>{description}</h3>
